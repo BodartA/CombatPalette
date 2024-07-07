9c7933800a8c1fc12e3e04ca12f1ea88973d7b91
BodartA <a.bodart23@gmail.com>
2024-07-07 19:50:26 +0200

    Navbar done and Buttons component modified to be optimized

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,4 +1,7 @@
-import { ArrowRight } from "lucide-react";
+import { ReadStream } from "fs";
+import { ArrowRight, Component } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Button = ({ name = "Button" }) => {
   return (
@@ -10,25 +13,45 @@ const Button = ({ name = "Button" }) => {
 
 export default Button;
 
-export const SmallPrimaryButton = ({ name = "smallButton" }) => {
+export const PrimaryButtonWIcon = ({ name = "smallButton", link = "/", icon = <ArrowRight className="flex my-auto w-[20px] h-[20px]" />, buttonSize = "small" }) => {
+
+  let mesures = ""
+
+  switch (buttonSize) {
+    case "small":
+      mesures = "px-6 py-[18px] text-base font-semibold"
+      break
+    case "default":
+      mesures = "lg:px-9 lg:py-6 px-6 py-[18px] lg:text-lg text-base font-semibold "
+      break
+    default:
+      mesures = "px-6 py-[18px] text-base font-semibold"
+      break
+  }
+
   return (
     <div>
-      <button className=" bg-back-purple text-white px-6 py-[18px] rounded-full hover:text-back-purple hover:bg-white border-[1px] border-back-purple transition duration-300 text-base font-semibold text-nowrap font-dm-sans">
-        {name}
+      <button className={`flex bg-emerald-800 text-white ${mesures} rounded-full hover:text-emerald-800 hover:bg-white border-[1px] border-emerald-800 transition duration-300 text-nowrap font-dm-sans gap-2`}>
+        <Link href={link}>{name}</Link>
+        {icon}
       </button>
     </div>
   );
 };
 
-export const DefaultPrimaryButtonArrowResp = ({
-  name = "defaultButtonWIcon",
-}) => {
+
+
+/*
+
+export const DefaultPrimaryButtonArrowResp = ({ name = "defaultButtonWIcon", link = "/", icon = <ArrowRight /> }) => {
   return (
     <div className=" inline-block">
-      <button className=" flex bg-back-purple text-white lg:px-9 lg:py-6 px-6 py-[18px] rounded-full hover:text-back-purple hover:bg-white border-[1px] border-back-purple transition duration-300 lg:text-lg text-base font-semibold text-center text-nowrap font-dm-sans gap-2">
-        {name}
-        <ArrowRight className=" flex my-auto w-[20px] h-[20px]" />
+      <button className=" flex bg-emerald-800 text-white lg:px-9 lg:py-6 px-6 py-[18px] rounded-full hover:text-emerald-800 hover:bg-white border-[1px] border-emerald-800 transition duration-300 text-center text-nowrap font-dm-sans gap-2">
+        <Link href={link}>{name}</Link>
+        {icon}
       </button>
     </div>
   );
 };
+
+*/
